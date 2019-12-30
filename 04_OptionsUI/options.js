fd3fc66813e0580c352e70622e0f116f7454c20e
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-30 16:20:53 +0900

    Set initial color value before set its

--- 04_OptionsUI/options.js
+++ 04_OptionsUI/options.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const saveBtn = document.querySelector('#save');
+const colors = document.querySelector('#colors');
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get({
+    color: 'red'
+  }, (item) => {
+    colors.value = item.color;
+  });
+});
 
 saveBtn.addEventListener('click', function() {
-  const color = document.querySelector('#colors').value;
+  const color = colors.value;
   chrome.storage.sync.set({ color }, () => {
     console.log('saved: ' + color);
   });
