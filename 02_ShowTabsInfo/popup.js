b1aeb11d85abc4a6f78d80869c42ce9c75201e4f
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-30 15:34:31 +0900

    Enable select lastFocuedWindow option

--- 02_ShowTabsInfo/popup.js
+++ 02_ShowTabsInfo/popup.js
@@ -1,9 +1,20 @@
 'use strict';
 
-chrome.tabs.query({}, function(tabs) {
-  const results = document.querySelector('#results');
-  const titles = tabs.map(tab => tab.title);
+function showTabsInfo({ lastFocusedWindow = false }) {
+  chrome.tabs.query({ lastFocusedWindow }, function(tabs) {
+    const results = document.querySelector('#results');
+    const titles = tabs.map(tab => tab.title);
 
-  results.value = titles.join('\n');
-  results.select();
+    results.value = titles.join('\n');
+    results.select();
+  });
+}
+
+showTabsInfo({ lastFocusedWindow: false });
+
+// user can select lastFocusedWindow option
+const lastFocusedWindow = document.querySelector('#lastFocusedWindow');
+
+lastFocusedWindow.addEventListener('click', function(e) {
+  showTabsInfo({ lastFocusedWindow: e.target.checked });
 });
