a39e78cd90d851e633040f4bf1ee0812345b15dd
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-30 15:38:47 +0900

    Enable select active option

--- 02_ShowTabsInfo/popup.js
+++ 02_ShowTabsInfo/popup.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function showTabsInfo({ lastFocusedWindow = false }) {
-  chrome.tabs.query({ lastFocusedWindow }, function(tabs) {
+function showTabsInfo({ lastFocusedWindow = false, active = false }) {
+  chrome.tabs.query({ lastFocusedWindow, active }, function(tabs) {
     const results = document.querySelector('#results');
     const titles = tabs.map(tab => tab.title);
 
@@ -18,3 +18,13 @@ const lastFocusedWindow = document.querySelector('#lastFocusedWindow');
 lastFocusedWindow.addEventListener('click', function(e) {
   showTabsInfo({ lastFocusedWindow: e.target.checked });
 });
+
+
+// user can select active option
+
+const active = document.querySelector('#active');
+
+active.addEventListener('click', function(e) {
+  showTabsInfo({ active: e.target.checked, lastFocusedWindow: true });
+});
+
